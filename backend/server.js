93bac4294944cda4d71ce3beb4cd503b323afe99
RahuRabh <rahurabh@gmail.com>
2024-05-18 10:58:43 +0530

    initial file setup

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+require("dotenv").config()
+
+const express = require("express")
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
+
+const mongoose = require("mongoose")
+
+// const auth = require("./routes/auth")
+// const quiz = require("./routes/quiz")
+// const errorHandler = require("./middleware/errorHandler")
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(cookieParser())
+
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log("MONGO CONNECTED"))
+    .catch((err) => console.log(err))
+
+// app.use("/api/auth", auth)
+// app.use("/api/quiz", quiz)
+// app.use("/*", (req, res) => {
+//     res.status(404).json({ errorMessage: "Route Not found" })
+// })
+// app.use("/", errorHandler)
+app.use("/", (req, res) => {
+    res.status(404).json({ message: "Server is up and running" })
+})
+
+const HOST = process.env.HOST || "localhost"
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Backend Server is running at http://${HOST}:${PORT}`)
+})
