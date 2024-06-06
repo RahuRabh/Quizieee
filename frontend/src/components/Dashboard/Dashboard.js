c05543b25a7c8965116810f2eeabb97ff068f764
RahuRabh <rahurabh@gmail.com>
2024-06-06 11:54:00 +0530

    stlying fixed

--- frontend/src/components/Dashboard/Dashboard.js
+++ frontend/src/components/Dashboard/Dashboard.js
@@ -1,78 +1,3 @@
-// import React, { useEffect, useState } from "react";
-
-// import styles from "./Dashboard.module.css";
-// import eye from "../../assets/eye.png";
-
-// import { getQuizByUser } from '../../apis/quiz';
-
-// import { formatNumber } from '../../utils/formatNumber'
-// import { convertDate} from '../../utils/convertDate'
-
-// import Loader from '../../components/Loader/Loader'
-
-// export default function Dashboard() {
-//   const [quizData, setQuizData] = useState(null);
-//   const userId = localStorage.getItem('userId');
-
-//   useEffect(() => {
-//     const fetchQuiz = async () => {
-//       try {
-//         const response = await getQuizByUser(userId);
-//         setQuizData(response);
-//       } catch (error) {
-//         console.error('Error fetching quiz:', error);
-//       }
-//     };
-
-//     fetchQuiz();
-//   }, [userId]);
-
-//   if (!quizData) return <div><Loader /></div>;
-
-//   const { dashboardData, quizAnalytics } = quizData;
-
-//   return (
-//     <div className={styles.main}>
-//       <div className={styles.heading}>
-//         <div className={styles.quiz}>
-//           <p className={styles.no}>{dashboardData.totalQuizzes}</p>
-//           <p className={styles.title}>Quiz</p>
-//           <p className={styles.text}>Created</p>
-//         </div>
-//         <div className={styles.questions}>
-//           <p className={styles.no}>{dashboardData.totalQuestions}</p>
-//           <p className={styles.title}>questions</p>
-//           <p className={styles.text}>Created</p>
-//         </div>
-//         <div className={styles.impressions}>
-//           <p className={styles.no}>{formatNumber(dashboardData.totalImpression)}</p>
-//           <p className={styles.title}>Total</p>
-//           <p className={styles.text}>Impressions</p>
-//         </div>
-//       </div>
-//       <div className={styles.content}>
-//         <h2>Trending Quizzes</h2>
-//         <div className={styles.quizList}>
-//           {quizAnalytics.map((quiz, index) => (
-//             <div key={index} className={styles.tquiz}>
-//             <div className={styles.container}>
-//               <p className={styles.name}>{quiz.name}</p>
-//               <p className={styles.eye}>
-//                 {formatNumber(quiz.impression)}
-//               </p>
-//               <img className={styles.eyeIcon} alt="eye" src={eye} />
-//               </div>
-//               <div>
-//               <p className={styles.date}>{convertDate(quiz.createdAt)}</p>
-//               </div>
-//             </div>
-//           ))}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
 import React, { useEffect, useState } from "react";
 import styles from "./Dashboard.module.css";
 import eye from "../../assets/eye.png";
@@ -129,11 +54,19 @@ export default function Dashboard() {
           {sortedQuizAnalytics.map((quiz, index) => (
             <div key={index} className={styles.tquiz}>
               <div className={styles.container}>
+              <div>
                 <p className={styles.name}>{quiz.name}</p>
+              </div>  
+                <div className={styles.orange}>
+                <div>
                 <p className={styles.eye}>
                   {formatNumber(quiz.impression)}
                 </p>
+                </div>
+                <div>
                 <img className={styles.eyeIcon} alt="eye" src={eye} />
+              </div>
+              </div>
               </div>
               <div>
                 <p className={styles.date}>{convertDate(quiz.createdAt)}</p>
